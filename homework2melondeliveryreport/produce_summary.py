9441d5699fe9b1c2473c4d165768a7acf9d1bf32
doubledherin <doubledherin@gmail.com>
2014-09-30 21:31:01 -0700

    Made the report print in all uppercase letters.

--- homework2melondeliveryreport/produce_summary.py
+++ homework2melondeliveryreport/produce_summary.py
@@ -3,7 +3,6 @@ def main():
 
     def print_report(day):
 
-        # Day 1
         print "Day %d" % day
         if day == 1:
             my_file = open("um-deliveries-20140519.csv")
@@ -14,17 +13,21 @@ def main():
 
         for line in my_file:
             line = line.rstrip()
+            line_upper = line.upper()
             words = line.split(',')
             
             melon = words[0]
             count = words[1]
             amount = words[2]
             
-            print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
+            line_out = "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
+            line_out_upper = line_out.upper()
+            print line_out_upper
         my_file.close()
         print "\n",
 
-    day = int(raw_input('What day would you like to print: 1, 2, or 3? > '))
+    day = int(raw_input('What day would you like to print: 1, 2, or 3? > '.upper()))
+    print type(day)
 
     print_report(day)
 

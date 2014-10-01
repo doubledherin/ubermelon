0e5d3c0dd2645ae387e0e62505a54f7dfc86b624
doubledherin <doubledherin@gmail.com>
2014-09-30 21:23:23 -0700

    Encapsulated code into print_report(day) function.

--- homework2melondeliveryreport/produce_summary.py
+++ homework2melondeliveryreport/produce_summary.py
@@ -1,52 +1,34 @@
 def main():
 
-    # Day 1
-    print "Day 1"
-    my_file = open("um-deliveries-20140519.csv")
 
-    for line in my_file:
-        line = line.rstrip()
-        words = line.split(',')
-        
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
-        
-        print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
-    my_file.close()
-    print "\n",
+    def print_report(day):
 
-    # Day 2
-    print "Day 2"
-    my_file = open("um-deliveries-20140520.csv")
+        # Day 1
+        print "Day %d" % day
+        if day == 1:
+            my_file = open("um-deliveries-20140519.csv")
+        elif day == 2:
+            my_file = open("um-deliveries-20140520.csv")
+        elif day == 3:
+            my_file = open("um-deliveries-20140521.csv")
 
-    for line in my_file:
-        line = line.rstrip()
-        words = line.split(',')
-        
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
-        
-        print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
-    my_file.close()
-    print "\n",
+        for line in my_file:
+            line = line.rstrip()
+            words = line.split(',')
+            
+            melon = words[0]
+            count = words[1]
+            amount = words[2]
+            
+            print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
+        my_file.close()
+        print "\n",
 
-    # Day 3
-    print "Day 3"
-    my_file = open("um-deliveries-20140521.csv")
+    day = int(raw_input('What day would you like to print: 1, 2, or 3? > '))
 
-    for line in my_file:
-        line = line.rstrip()
-        words = line.split(',')
-        
-        melon = words[0]
-        count = words[1]
-        amount = words[2]
-        
-        print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
-    my_file.close()
-    print "\n",
+    print_report(day)
+
+
 
 if __name__ == "__main__":
     main()

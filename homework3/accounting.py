c04680a467a134bb5101b3d667ce087a9cc9994d
doubledherin <doubledherin@gmail.com>
2014-10-01 20:59:43 -0700

    Added $ to dollar amounts.

--- homework3/accounting.py
+++ homework3/accounting.py
@@ -17,7 +17,7 @@ def revenue_per_melon():
         price = melon_prices[melon_type]
         revenue = price * melon_tallies[melon_type]
         total_revenue += revenue
-        print "We sold %d %s melons at %0.2f each for a total of %0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
+        print "We sold %d %s melons at $%0.2f each for a total of $%0.2f" % (melon_tallies[melon_type], melon_type, price, revenue)
     print "******************************************"
 
 def sales_report_bifurcated():
@@ -29,8 +29,8 @@ def sales_report_bifurcated():
             sales[0] += float(data[3])
         else:
             sales[1] += float(data[3])
-    print "Salespeople generated %0.2f in revenue." % sales[1]
-    print "Internet sales generated %0.2f in revenue." % sales[0]
+    print "Salespeople generated $%0.2f in revenue." % sales[1]
+    print "Internet sales generated $%0.2f in revenue." % sales[0]
     if sales[1] > sales[0]:
         print "Guess there's some value to those salespeople after all."
     else:
